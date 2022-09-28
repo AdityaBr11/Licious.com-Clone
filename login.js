c9bf2ebc857c402bc2b9306d06b43cb1b274679e
shivam061095 <shivam061095@gmail.com>
2022-09-28 22:50:08 +0530

    day2

---
+++ login.js
@@ -0,0 +1,47 @@
+
+let store=[{
+    username:"shivam",
+    password:"12345"
+},
+{
+    username:"satyam",
+    password:"88558"
+},
+{
+    username:"vikash",
+    password:"480136"
+}
+
+]
+
+
+
+const getDetails =()=>{
+    event.preventDefault()
+    username=document.getElementById("username").value;
+    password=document.getElementById("password").value;
+    let flag=false;
+    for(let i=0;i<store.length;i++){
+        if(store[i].username==username&&store[i].password==password){
+            flag=true;
+        }
+    }
+    if(flag==true){
+        alert("log in succefull")
+    }
+    else{
+        alert("something is wrong")
+    }
+    // console.log(username,password)
+    // store.forEach(function(ele){
+    //     if(ele.username==username&& ele.password==password){
+    //         console.log("log in succesfully")
+    //         return
+        
+    //     }
+        
+    // })
+
+
+    
+}
