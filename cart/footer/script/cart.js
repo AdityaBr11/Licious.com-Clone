9cd3fbb667a6c2f9a9607bf73b953610e23926bd
ankitkumarthakur3461 <ankitthakur3461@gmail.com>
2022-10-01 00:43:36 +0530

    fp04_435_day_4

--- cart/footer/script/cart.js
+++ cart/footer/script/cart.js
@@ -1,6 +1,7 @@
 function openNav() {
     let cart_div= document.getElementById("mySidenav")
    cart_div.style.width = "30%";
+   document.getElementById("cut").style.marginLeft="-46px"
    
    
    cart_div.style.backgroundColor="white"
@@ -16,22 +17,26 @@ function openNav() {
        {
            Image:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/eb0de06a-d96a-c786-1de9-1bd9cadbb89b/original/ChickenLollipopPiecesHeroShot.jpg",
            title:"Chicken Lollipop - Pack of 10",
-           price:"10Pieces | 166 175",
+           price:"10Pieces |  ₹166  ",
+           s:"₹175",
        },
        {
            Image:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/1803f01a-2f09-e768-897e-7c9707e5c5ed/original/Classic-Eggs---Pack-Of-30-Hero-Shot.jpg",
            title:"Classic Eggs - Pack Of 30",
-           price:"30Pieces | 280 295",
+           price:"30Pieces |  ₹280 ",
+           s:"₹310",
        },
        {
            Image:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/4d08a6f1-998d-66c8-16e7-fb9efec195ed/original/Afghani-Murgh-Seekh-Kebab(1).jpg",
            title:"Afghani Murgh Seekh Kebab | Ready in 8 Minutes",
-           price:"4Pieces | 256 269",
+           price:"4Pieces |  ₹256",
+           s:"₹300",
        },
        {
            Image:"https://dao54xqhg9jfa.cloudfront.net/OMS-ProductMerchantdising/715d4156-cae5-f3c5-ef78-a6e5c129ed5c/original/Lucknowi-Mutton-Galouti-Kebab-Hero-Shot-Uncooked_(1).jpg",
            title:"Lucknowi Mutton Galouti Kebab | Ready in 8 Minutes",
-           price:"6Pieces | 359 399",
+           price:"6Pieces |  ₹359",
+           s:"₹380",
        },
    ]
    
@@ -46,11 +51,18 @@ function openNav() {
           let title=document.createElement("h5");
           title.innerText=arr[i].title;
          
-          
+          let price_div=document.createElement("div");
+          price_div.setAttribute("id","st")
    
           let price=document.createElement("p");
           price.innerText=arr[i].price;
           
+
+          let strick=document.createElement("strike");
+          strick.innerText=arr[i].s
+          strick.setAttribute("id","str");
+
+          price_div.append(price,strick);
    
           let btn=document.createElement("button");
           btn.innerText="ADD";
@@ -58,8 +70,14 @@ function openNav() {
        
         
    
-          new_div.append(image,title,price,btn);
+          new_div.append(image,title,price_div,btn);
           document.getElementById("product").append(new_div)
+   }
+
+   function cut()
+   {
+    document.getElementById("mySidenav").style.width="0%"
+    document.getElementById("cut").style.marginLeft="46px"
    }
    
    
